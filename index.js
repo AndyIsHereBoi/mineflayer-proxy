2053da822545223e5d2d2a580fa63f3c3a234c81
AndyIsHereBoi <andyaaron2ndgrade@gmail.com>
2026-02-23 19:02:20 -0600

    switch to .env

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+require('dotenv').config()
 const { createBot } = require('mineflayer')
 const socks = require('socks').SocksClient
-const config = require('./config.json')
 
 const bot = createBot({
-  username: config.mcUsername,
-//   password: config.mcPassword,
-  auth: config.mcAuthType,
-  host: config.mcServerHost,
-  port: config.mcServerPort,
+  username: process.env.MC_USERNAME,
+//   password: process.env.MC_PASSWORD,
+  auth: process.env.MC_AUTH_TYPE,
+  host: process.env.MC_SERVER_HOST,
+  port: parseInt(process.env.MC_SERVER_PORT),
   connect: (client) => {
     socks.createConnection({
       proxy: {
-        host: config.proxyHost,
-        port: config.proxyPort,
+        host: process.env.PROXY_HOST,
+        port: parseInt(process.env.PROXY_PORT),
         type: 5,
-        userId: config.proxyUsername,
-        password: config.proxyPassword
+        userId: process.env.PROXY_USERNAME,
+        password: process.env.PROXY_PASSWORD
       },
       command: 'connect',
       destination: {
-        host: config.mcServerHost,
-        port: config.mcServerPort
+        host: process.env.MC_SERVER_HOST,
+        port: parseInt(process.env.MC_SERVER_PORT)
       }
     }, (err, info) => {
       if (err) {
